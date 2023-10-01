11ff4f7b1fe0411b5109d75170c1811119e3954d
Richard Song <rich@Richards-MBP.lan>
2023-10-01 16:48:58 -0700

    Added prompt to ask user how many characters to have in password

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,15 @@
 // Assignment code here
+//Prompt User Input and ask how many characters for password
+//Get prompt value, use value to dictate the length of password and replace with char.length in for-loop below
+
+const requestedChar = window.prompt("How many characters do you want in your password?")
+
 function generatePassword() {
   let newPassword = ""
   const char = "#$%&'()*+,-./:;<=>?@[^_`{|}~"
 
 
-  for (i = 0; i < char.length; i++) {
+  for (i = 0; i < requestedChar; i++) {
     const randomIndex = Math.floor(Math.random() * char.length);
     newPassword += char[randomIndex];
   }
