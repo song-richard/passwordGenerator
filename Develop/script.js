9921b07a168ee41109926957ea77f5680414d769
Richard Song <rich@Richards-MBP.lan>
2023-10-08 23:48:28 -0700

    Reorganized directory structure. Cleaned up JS file

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,35 @@
-// Assignment code here
-//Prompt User Input and ask how many characters for password
-//Get prompt value, use value to dictate the length of password and replace with char.length in for-loop below
+let requestedChar;
 
-const requestedChar = window.prompt("How many characters do you want in your password?")
+do {
+  requestedChar = parseInt(window.prompt("How many characters do you want in your password?"));
+  if (isNaN(requestedChar)) {
+    alert("Please enter a valid number for password length.");
+  } else if (requestedChar < 8 || requestedChar > 128) {
+    alert("Password length must be between 8 and 128 characters.");
+  }
+} while (isNaN(requestedChar) || requestedChar < 8 || requestedChar > 128);
+
+
+const specialCharPrompt = window.prompt("Do you want special characters in your password?").toLowerCase()
+const numericPrompt = window.prompt("Do you want to include numeric characters?").toLowerCase()
+const lowerUpperPrompt = window.prompt("Do you want lowercase or uppercase characters?").toLowerCase()
+
 
 function generatePassword() {
-  let newPassword = ""
-  const char = "#$%&'()*+,-./:;<=>?@[^_`{|}~"
+  let newPassword = "";
+  let char = "abcdefghijklmnopqrstuvwxyz"; 
 
+  if (lowerUpperPrompt === "uppercase") {
+    char = char.toUpperCase(); 
+  }
+
+  if (specialCharPrompt === "yes") {
+    char += "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
+  }
+
+  if (numericPrompt === "yes") {
+    char += "0123456789";
+  }
 
   for (i = 0; i < requestedChar; i++) {
     const randomIndex = Math.floor(Math.random() * char.length);
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Password Generator Starter Code
+## Description
+
+The following password generator prompts the end-user by requesting how many characters they would like in their password, if they would like to include special characters, numeric characters, lowercase characters, or uppercase characters.
+
+The password generator will provide a randomized password based on their selections above.
+
+## Link to GitHub Repository:
+
+https://github.com/song-richard/passwordGenerator
+
+## Usage
+
+Screenshot:
